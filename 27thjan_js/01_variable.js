513f87cb33fe38e77ba26377edd5cca23527e038
shaikhzo22 <153588499+shaikhzo22@users.noreply.github.com>
2024-01-27 13:32:03 +0000

    container config done

---
+++ 27thjan_js/01_variable.js
@@ -0,0 +1,17 @@
+const studentRollno = 123
+let stuEmail = "abc@gmail.com"
+var stuPsswd = "college"
+studCity = "mumbai"
+let studState;
+
+// studentRollno = 1//
+console.log(studentRollno);
+
+stuEmail= "xyz@gmail.com"
+console.log(stuEmail);
+
+console.table([studentRollno, stuEmail, stuPsswd,studState,])
+
+/*
+prefer not to use var bcoz of issue in blockscope & functional scope
+*/
---
+++ 27thjan_js/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict ";// treat all js code as newer version
+// alert(3+3) // we are using nodejs, not browser hence this will not work as it works in browser
+console.log(3+3); // code creditibility should be high
+
+let sname ="zobiya"
+ let age =19
+ let isLoggedIn =false
+ let state;
+console.log(typeof isLoggedIn);
+console.log(typeof sname);
+console.log(typeof state);
